223cf66ee3aafd17bf1abaac323fd98c069d5722
cnb <wu15766384313@163.com>
2026-07-27 21:29:24 +0800

    chore: remove obsolete project artifacts

--- product-drill-app/src/app/demov3-workbench/page.tsx
+++
@@ -1,5 +0,0 @@
-import { AppShellV3Workbench } from "../app-shell-v3-workbench";
-
-export default function DemoV3WorkbenchPage() {
-  return <AppShellV3Workbench />;
-}
--- product-drill-mvp/apps/web/src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-﻿import { ProductDrillApp } from "./product-drill-app";
-
-export default function HomePage() {
-  return <ProductDrillApp />;
-}
